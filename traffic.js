682cbaa4b11f137225d89c6878c5ea280720efe6
daniscodes <dan97@bell.net>
2023-02-07 14:32:10 -0500

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -14,3 +14,18 @@ dirLight.position.set(100, -300, 400);
 scene.add(dirLight);
 
 //set up camera
+const aspectRatio = window.innerWidth / window.innerHeight;
+const cameraWidth = 150;
+const cameraHeight = cameraWidth / aspectRatio;
+
+const camera = new THREE.OrthographicCamera(
+  cameraWidth / -2, //left
+  cameraWidth / 2, //right
+  cameraHeight / 2, //top
+  cameraHeight / -2, //bottom
+  0, //near plane
+  1000 //far plane
+);
+camera.position.set(200, -200, 300);
+camera.up.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
